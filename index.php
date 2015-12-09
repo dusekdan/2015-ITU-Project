a8e39cebc5276eb8b223159f8dc3b4160866074a
DusekDan <dusekdan@gmail.com>
2015-12-09 20:31:10 +0100

    shitfacecommit.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <h3 class="fs-subtitle">This is step 1</h3>
         <input type="text" name="email" placeholder="Email" />
         <input type="password" name="pass" placeholder="Password" />
-        <input type="password" name="cpass" placeholder="Confirm Password" />
+        <input type="number" name="cpass" placeholder="Confirm Password" />
         <input type="button" name="next" class="next action-button" value="Next" />
     </fieldset>
     <fieldset>
